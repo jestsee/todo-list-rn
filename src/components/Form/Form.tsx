4d1d0cd8eacb47aa64ba880b43d3239588658d04
Jesica <13519011@std.stei.itb.ac.id>
2023-01-13 18:55:47 +0700

    fix: type error

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,46 +1,58 @@
-import { Control, Controller } from 'react-hook-form'
+/* eslint-disable sort-keys */
+/* eslint-disable indent */
+import {
+  FieldPath,
+  FieldValues,
+  UseControllerProps,
+  useController
+} from 'react-hook-form'
 import { Text, TextInput, View } from 'react-native'
 import { FieldError } from 'react-hook-form'
 import { capitalize } from 'src/utils'
 import { styles } from './styles'
 
-export interface Props {
-  control: Control
-  name: string
+type FormProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>
+> = UseControllerProps<TFieldValues, TName>
+
+export type Props<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>
+> = {
   placeholder?: string
   error?: FieldError
   label?: string
   suffix?: React.ReactNode
   secureTextEntry?: boolean
-}
+} & FormProps<TFieldValues, TName>
 
-export const Form: React.FC<Props> = ({
-  placeholder,
-  error,
-  label,
-  suffix,
-  secureTextEntry,
-  ...rest
-}) => {
+export const Form = <
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>
+>(
+  props: Props<TFieldValues, TName>
+) => {
+  const { placeholder, error, label, suffix, secureTextEntry, name, control } =
+    props
+  const {
+    field: { onBlur, onChange, value }
+  } = useController({ name, control })
+
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>{label ?? capitalize(rest.name)}</Text>
-      <Controller
-        {...rest}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <View style={styles.form}>
-            <TextInput
-              style={styles.input}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-              placeholder={placeholder}
-              secureTextEntry={secureTextEntry}
-            />
-            {suffix}
-          </View>
-        )}
-      />
+      <Text style={styles.label}>{label ?? capitalize(name)}</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          onBlur={onBlur}
+          onChangeText={onChange}
+          value={value}
+          placeholder={placeholder}
+          secureTextEntry={secureTextEntry}
+        />
+        {suffix}
+      </View>
       {error && <Text style={styles.error}>{error.message}</Text>}
     </View>
   )
