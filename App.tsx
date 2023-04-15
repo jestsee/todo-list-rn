dfaa9221104ec37e04d4b17e7f3626df9d40dada
Jesica <13519011@std.stei.itb.ac.id>
2023-04-15 15:18:12 +0700

    feat: notification setup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,56 @@
 import 'react-native-url-polyfill/auto'
 // TODO import * as Linking from 'expo-linking'
+import * as Notifications from 'expo-notifications'
+import { useEffect, useRef, useState } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { Provider } from 'react-redux'
 import { Routes } from './src/routes'
 import { Snackbar } from '@components'
+import { Subscription } from 'expo-modules-core'
+import { registerForPushNotificationsAsync } from 'src/notification'
 import { store } from '@redux/store'
 import { useContact } from '@hooks/useContact'
-import { useEffect } from 'react'
 import { useLocation } from '@hooks/useLocation'
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true
+  })
+})
 export default function App() {
   const { requestLocationPermission } = useLocation()
   const { requestContactPermission } = useContact()
 
+  // notification
+  const [notification, setNotification] = useState<Notifications.Notification>()
+  const notificationListener = useRef<Subscription>()
+  const responseListener = useRef<Subscription>()
+
   useEffect(() => {
     requestLocationPermission()
     requestContactPermission()
+    registerForPushNotificationsAsync()
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification)
+      })
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response)
+      })
+
+    return () => {
+      if (notificationListener.current)
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        )
+      if (responseListener.current)
+        Notifications.removeNotificationSubscription(responseListener.current)
+    }
   }, [])
 
   return (
---
+++ src/notification.ts
@@ -0,0 +1,24 @@
+import * as Notifications from 'expo-notifications'
+import { Platform } from 'react-native'
+
+export const registerForPushNotificationsAsync = async () => {
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C'
+    })
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync()
+  let finalStatus = existingStatus
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync()
+    finalStatus = status
+  }
+  if (finalStatus !== 'granted') {
+    return
+  }
+  console.log('Notification permission granted')
+}
